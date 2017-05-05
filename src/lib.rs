8e891bd1f5a38bd57c757d87afa9a6eb1334c7ea
Felix S. Klock II <pnkfelix@pnkfx.org>
2017-05-05 16:10:54 +0200

    update deps to more recent versions. See https://url.spec.whatwg.org/#userinfo-encode-set for how I selected encode set; note that we need (at least) to encode `=`.

--- src/lib.rs
+++ src/lib.rs
@@ -660,7 +660,7 @@ mod rs2md;
 fn encode_to_url(code: &str) -> String {
     use url::percent_encoding as enc;
     // let new_code: String = enc::utf8_percent_encode(code.trim(), enc::QUERY_ENCODE_SET);
-    let new_code: String = enc::utf8_percent_encode(code.trim(), enc::FORM_URLENCODED_ENCODE_SET);
+    let new_code: String = enc::utf8_percent_encode(code.trim(), enc::USERINFO_ENCODE_SET).collect();
     format!("https://play.rust-lang.org/?code={}&version=nightly", new_code)
 }
 
